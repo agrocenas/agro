8963ae190daedf6823bfa7d0f01d69aca80a828d
tferreira <namasteadmin@gmail.com>
2013-10-20 19:21:40 +0100

    demo version

--- agro/public/js/utils.js
+++ agro/public/js/utils.js
@@ -53,7 +53,83 @@ var SignUp = {
         }
         $("#registerForm")[0].submit();
     }
+<<<<<<< 39318ea86a633c3309261965bfde3c11ebd7d2aa
 }
+=======
+};
+
+var customIcons = {
+    user: {
+        icon: 'http://labs.google.com/ridefinder/images/mm_20_green.png',
+        shadow: 'http://labs.google.com/ridefinder/images/mm_20_shadow.png'
+    },
+    supplier: {
+        icon: 'http://labs.google.com/ridefinder/images/mm_20_red.png',
+        shadow: 'http://labs.google.com/ridefinder/images/mm_20_shadow.png'
+    }
+};
+
+var Map = {
+    downloadUrl: function(url, callback) {
+        var request = window.ActiveXObject ?
+                new ActiveXObject('Microsoft.XMLHTTP') :
+                new XMLHttpRequest;
+
+        request.onreadystatechange = function() {
+            if (request.readyState == 4) {
+                request.onreadystatechange = Map.doNothing();
+                callback(request, request.status);
+            }
+        };
+
+        request.open('GET', url, false);
+        request.send(null);
+    }
+    ,
+    bindInfoWindow: function(maplocation, map, infoWindow, html) {
+        google.maps.event.addListener(maplocation, 'click', function() {
+            infoWindow.setContent(html);
+            infoWindow.open(map, maplocation);
+        });
+    }
+    ,
+    doNothing: function() {
+    }
+    ,
+    loadMap: function() {
+        var map = new google.maps.Map(document.getElementById("map"), {
+            center: new google.maps.LatLng(38.8407269,-9.1602799),
+            zoom: 6,
+            mapTypeId: 'roadmap'
+        });
+        var infoWindow = new google.maps.InfoWindow;
+
+        Map.downloadUrl("../genMapLocationsXML.php", function(data) {
+            var xml = data.responseXML;
+            var maplocations = xml.documentElement.getElementsByTagName("maplocation");
+            for (var i = 0; i < maplocations.length; i++) {
+                var name = maplocations[i].getAttribute("name");
+                var address = maplocations[i].getAttribute("address");
+                var type = maplocations[i].getAttribute("type");
+                var point = new google.maps.LatLng(
+                        parseFloat(maplocations[i].getAttribute("lat")),
+                        parseFloat(maplocations[i].getAttribute("lng")));
+                var html = "<b>" + name + "</b> <br/>" + address;
+                var icon = customIcons[type] || {};
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: point,
+                    icon: icon.icon,
+                    shadow: icon.shadow
+                });
+                Map.bindInfoWindow(marker, map, infoWindow, html);
+            }
+        });
+    }
+};
+
+
+>>>>>>> a882c18ffeffb43c2614109c4a7714a980c928ae
 
 $(document).ready(function () {
     $("#registerForm .alert").hide();
