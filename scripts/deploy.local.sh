d78d4a4267afa33374eb7870fd4037b8bf3b678c
tferreira <tferreira@venusq>
2014-01-26 03:44:14 +0000

    script test

--- scripts/deploy.local.sh
+++ scripts/deploy.local.sh
@@ -5,12 +5,12 @@ sourcedir=../agro
 
 
 echo "deploy started..."
-sudo rm -r /var/www/agro
+rm -r /var/www/agro
 echo "directory removed"
-sudo cp -r $sourcedir $targetdir
+cp -r $sourcedir $targetdir
 echo "Contents copied !"  
-sudo chmod -R 777 /var/www
+chmod -R 777 /var/www
 echo "Perminssions set! "
-sudo service apache2 restart
+/etc/init.d/apache2 restart
 echo "apache2 restarted"
 echo "Deploy Succesfull ..." 
